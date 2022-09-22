633be9773699770b47dd4cb17cdfeb4cf4aa5399
misha-solntcev <misha.solntcev@gmail.com>
2022-09-22 13:35:45 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  Задача.
+ Найти два числа из массива, в сумме равных числу к.
+    Решение с помощью HashSet.  */
+
+namespace twoSum_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -3, 0, 2, 4, 5 };
+            int k = 6;
+            List<int> set = new List<int>();
+            int[] res = new int[] {0, 0};
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int numberToFind = k - nums[i];
+                if (set.Contains(numberToFind))
+                {
+                    res = new int[] { numberToFind, nums[i] };                    
+                }
+                set.Add(nums[i]);
+            }
+
+            Console.WriteLine($"[{res[0]}, {res[1]}]");
+            Console.ReadKey();
+
+
+        }
+    }
+}
